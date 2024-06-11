7815546cd7ba50a14bc73290e90958bef17a7ac3
ashokmorya707 <146889483+ashokmorya707@users.noreply.github.com>
2024-06-11 18:13:16 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,21 @@
+"use strict"; // treat all JS code as newer version
+
+// alert("hello") // we are using node.js not a browser
+
+// code readibility should be high
+
+//   tc39.es   ECMA   mdn
+
+// number => 
+// bigint
+// string => " "
+// boolean
+// null => standalone value
+// undefined
+// symbol => unique
+
+
+// object 
+
+console.log(typeof null)
+
--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,18 @@
-console.log("ashok")
+const accountId = 14453;
+let accountEmail = "ashokmorya@gmail.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+
+// accountId = 2
+accountEmail = "ashok@gmail.com"
+accountCity = "Delhi"
+accountPassword = "4321"
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword])
+
+/*
+Prefer not to use var
+because of block problem......
+*/
